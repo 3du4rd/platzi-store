96df8028e7d1720db789a2f298061acad9f35b5c
“Eduard <3du4rd@gmail.com>
2021-06-16 12:51:26 -0500

    feat: Uso de Inputs y Outputs - Ciclo de vida de los componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,4 +66,9 @@ export class AppComponent {
     this.items.splice(index,1);
   }
 
+  clickProduct(id: number){
+    console.log('Product');
+    console.log(id);
+  }
+
 }
---
+++ src/app/components/product.component.ts
@@ -0,0 +1,46 @@
+import { 
+    Component, 
+    Input, 
+    Output, 
+    EventEmitter, 
+    OnChanges, 
+    SimpleChange, 
+    SimpleChanges } from '@angular/core'
+
+import { Product } from '../product.model'
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html'
+})
+export class ProductComponent {
+    @Input() product: Product;
+    @Output() onAddCart: EventEmitter<any> = new EventEmitter();
+
+    constructor(){
+        console.log('1.Constructor');
+    }
+
+    ngOnChanges(changes:SimpleChanges){
+        console.log('2. ngOnChanges');
+        console.log(changes);
+    }
+
+    // Buena practica hacer llamados a servicios de datos (APIS)
+    ngOnInit(){
+        console.log('3. ngOnInit');
+    }
+
+    ngDoCheck(){
+        console.log('4. ngDoCheck');
+    }
+
+    ngOnDestroy(){
+        console.log('5. ngOnDestroy');
+    }
+
+    addCart(){
+        console.log('Adicionar al carrito');
+        this.onAddCart.emit(this.product.id);
+    }
+}
