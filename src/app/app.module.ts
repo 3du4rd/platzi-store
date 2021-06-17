f256b4ca066ee75809e7238f6bf994979c7a7a63
Eduard Correa <3du4rd@gmail.com>
2021-06-16 19:22:15 -0500

    feat: Usando routerLink y routerActive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BannerComponent } from './banner/banner.component'
+import { BannerComponent } from './banner/banner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { BannerComponent } from './banner/banner.component'
     DemoComponent,
     HeaderComponent,
     FooterComponent,
-    BannerComponent
+    BannerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
