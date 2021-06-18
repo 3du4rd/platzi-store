0dddd5b075063e8024029dcdb62ec7b30990ca2a
“Eduard <3du4rd@gmail.com>
2021-06-17 22:14:48 -0500

    feat: Tarea - Crear formulario de contacto

--- src/app/contact/components/contact.component.ts
+++ src/app/contact/components/contact.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+//import { ConnectionService } from './connection.service';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ConnectionService } from '../services/connection.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,8 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
+  disabledSubmitButton: boolean = true;
+  optionsSelect: Array<any>;
 
-  constructor() { }
+  @HostListener('input') oninput() {
+
+  if (this.contactForm.valid) {
+    this.disabledSubmitButton = false;
+    }
+  }
+  
+  constructor() { 
+    this.contactForm = new FormGroup({
+      contactFormName: new FormControl('Nancy', Validators.minLength(2)),
+      contactFormEmail: new FormControl('Drew'),
+    });
+  }
+  //constructor(private fb: FormBuilder) {
+  /*constructor(private fb: FormBuilder, private connectionService: ConnectionService) {  
+  this.contactForm = fb.group({
+    'contactFormName': ['', Validators.required],
+    'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
+    'contactFormSubjects': ['', Validators.required],
+    'contactFormMessage': ['', Validators.required],
+    'contactFormCopy': [''],
+    });
+  }*/
+
+  onSubmit() {
+      console.log('Your message has been sent');
+      this.contactForm.reset();
+      this.disabledSubmitButton = true;
+    /*this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
+      alert('Your message has been sent.');
+      this.contactForm.reset();
+      this.disabledSubmitButton = true;
+    }, error => {
+      console.log('Error', error);
+    });*/
+  }  
 
   ngOnInit(): void {
     console.log('ngOnInit');
