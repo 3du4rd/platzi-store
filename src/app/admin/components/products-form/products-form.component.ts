b2df557c56d0445a99733bdac3e2717305b424f4
“Eduard <3du4rd@gmail.com>
2021-06-19 01:49:11 -0500

    feat: Ajustar estilos en un formulario

--- src/app/admin/components/products-form/products-form.component.ts
+++ src/app/admin/components/products-form/products-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/core/services/products/products.service';
 
 @Component({
   selector: 'app-products-form',
@@ -11,7 +13,9 @@ export class ProductsFormComponent implements OnInit {
   productForm: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
   ) { 
     this.buildForm();
   }
@@ -20,6 +24,10 @@ export class ProductsFormComponent implements OnInit {
     console.log('ProductsFormComponent');
   }
 
+  /**
+   * Construye el Formulario de Producto con los campos de
+   * Producto y sus validaciones
+   */
   private buildForm(){
     this.productForm = this.formBuilder.group({
       id: ['', [Validators.required]],
@@ -28,6 +36,25 @@ export class ProductsFormComponent implements OnInit {
       image: '',
       description: ['', [Validators.required]]
     });
+  }
+
+  /**
+   * Almacena el producto 
+   * * Consume el API Heroku
+   * TODO: construir el API Propia en Heroku
+   * @param event Evento del formulario
+   */
+  saveProduct(event: Event){
+    event.preventDefault();
+    if(this.productForm.valid){
+      const product = this.productForm.value;
+      this.productsService.createProduct(product).subscribe(
+        (newProduct) =>{
+          console.log(newProduct);
+          this.router.navigate(['./admin/products']);
+        }
+      );
+    }    
   }
 
 }
